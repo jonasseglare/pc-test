6153166b0866750163869ad6d91519971b91edc1
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-03-21 23:25:31 +0100

    asdf

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject pc-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["java/"]
+  :plugins [[lein-nodisassemble "0.1.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.clojure-goes-fast/clj-java-decompiler "0.1.0"]
+                 [proteus "0.1.6"]
+                 [primitive-math "0.1.6"]
+                 [insn "0.2.1"]
+                 [bluebell/utils "0.1.3-SNAPSHOT"]])
